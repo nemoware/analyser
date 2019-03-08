3da548eaa1ebdaf1d681b0b3ca9e0a207384a383
Artem Zaborskiy <compartia@gmail.com>
2019-03-08 13:01:57 +0800

    Fix unittest and embedder

--- embedding_tools.py
+++ embedding_tools.py
@@ -7,20 +7,22 @@ class AbstractEmbedder:
     def get_embedding_tensor(self, tokenized_sentences_list):
         pass
 
-    def embedd_tokenized_text(self, words):
+    def embedd_tokenized_text(self, words, lens):
         pass
 
     def embedd_sentence(self, str):
         words = tokenize_text(str)
-        return self.embedd_tokenized_text(words)
+        return self.embedd_tokenized_text([words], [len(words)])
 
     def embedd_contextualized_patterns(self, patterns):
         tokenized_sentences_list = []
         regions = {}
+
         i = 0
+        maxlen = 0
+        lens = []
         for (ctx_prefix, pattern, ctx_postfix) in patterns:
-            sentence = " "
-            sentence = sentence.join((ctx_prefix, pattern, ctx_postfix))
+            sentence = ' '.join((ctx_prefix, pattern, ctx_postfix))
 
             prefix_tokens = tokenize_text(ctx_prefix)
             pattern_tokens = tokenize_text(pattern)
@@ -35,23 +37,32 @@ class AbstractEmbedder:
 
             regions[i] = (start, end)
             tokenized_sentences_list.append(sentence_tokens)
+            lens.append(len(sentence_tokens))
+            if len(sentence_tokens) > maxlen:
+                maxlen = len(sentence_tokens)
+
             i = i + 1
 
+        print('maxlen=', maxlen)
         _strings = []
+
         for s in tokenized_sentences_list:
-            joined = ' '.join(s)
-            _strings.append(joined)
-            print (joined)
+            s.extend([' '] * (maxlen - len(s)))
+            _strings.append(s)
+            print (s)
+        _strings = np.array(_strings)
 
         ## ======== call TENSORFLOW -----==================
-        sentences_emb = self.get_embedding_tensor(_strings)
+        sentences_emb, wrds = self.embedd_tokenized_text(_strings, lens)
         ## ================================================
+
+        print (sentences_emb.shape)
+        #     assert len(sentence_tokens) == sentences_emb
 
         patterns_emb = []
 
         for i in regions:
             start, end = regions[i]
-            # print (start, end)
 
             sentence_emb = sentences_emb[i]
             pattern_emb = sentence_emb[start:end]
