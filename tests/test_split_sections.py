e5cec799c368ea3e6c7a678e6baa3de68817d76c
Artem Zaborskiy <compartia@gmail.com>
2019-03-16 16:43:13 +0800

    Fix unit tests

--- tests/test_split_sections.py
+++ tests/test_split_sections.py
@@ -14,7 +14,7 @@ from split import *
 class SplitSectionsTest(unittest.TestCase):
 
     def test_find_sections_indexes(self):
-        cd = ContractDocument()
+        cd = BasicContractDocument()
 
         cd.tokens = ['a', 'a', 'a', '\n', 'a', 'a', 'a']
         dists = np.array(
@@ -28,7 +28,7 @@ class SplitSectionsTest(unittest.TestCase):
         self.assertTrue(np.allclose(idxs, [[1, 0], [0, 3]]))
 
     def test_find_sentence_beginnings(self):
-        cd = ContractDocument()
+        cd = BasicContractDocument()
         cd.tokens = ['a', 'a', 'a', '\n', 'a', 'a', 'a', '\n', 'a', '\n', 'a']
         row_to_index = [6,1]
 
@@ -37,7 +37,7 @@ class SplitSectionsTest(unittest.TestCase):
         self.assertTrue(np.allclose(sentence_starts, [3, 0] ))
 
     def test_find_sentence_beginnings_2(self):
-        cd = ContractDocument()
+        cd = BasicContractDocument()
         cd.tokens = ['a', 'a', 'a', '\n', 'a', 'a', 'a', '\n', 'a', '\n', 'a']
         row_to_index = [6,1]
         sentence_starts = cd.find_sentence_beginnings(row_to_index)
@@ -45,7 +45,7 @@ class SplitSectionsTest(unittest.TestCase):
         self.assertTrue(np.allclose(sentence_starts, [3, 0]))
 
     def test_remove_near_indexes_1(self):
-        cd = ContractDocument()
+        cd = BasicContractDocument()
 
         indexes = [[0, 0], [0, 0], [0, 0]]
         filtered = cd.remove_similar_indexes(indexes, 0)
@@ -53,7 +53,7 @@ class SplitSectionsTest(unittest.TestCase):
         self.assertTrue(np.allclose(filtered, [[0, 0]]))
 
     def test_remove_near_indexes_2(self):
-        cd = ContractDocument()
+        cd = BasicContractDocument()
 
         indexes = [[100, 1], [10, 1], [500, 0]]
         filtered = cd.remove_similar_indexes(indexes, 1, min_section_size=1)
@@ -61,7 +61,7 @@ class SplitSectionsTest(unittest.TestCase):
         self.assertTrue(np.allclose(filtered, [[100, 1]]))
 
     def test_remove_near_indexes_3(self):
-        cd = ContractDocument()
+        cd = BasicContractDocument()
 
         indexes = [[10, 3000], [100, 2000], [500, 1000]]
         filtered = cd.remove_similar_indexes(indexes, 0, min_section_size=91)
@@ -69,7 +69,7 @@ class SplitSectionsTest(unittest.TestCase):
         self.assertTrue(np.allclose(filtered, [[10, 3000], [500, 1000]]))
 
     def test_remove_near_indexes_3(self):
-        cd = ContractDocument()
+        cd = BasicContractDocument()
 
         indexes = [[10, 3000], [100, 2000], [500, 1000]]
         filtered = cd.remove_similar_indexes(indexes, 0, min_section_size=1)
