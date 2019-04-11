ba2b4b462bd08529e2deb2fed7dd73f2cff7417b
Artem Zaborskiy <compartia@gmail.com>
2019-04-11 16:31:20 +0800

    tokenization

--- doc_structure.py
+++ doc_structure.py
@@ -212,7 +212,11 @@ class DocumentStructure:
     for row in lines:
       line_number += 1
 
-      line_tokens_cc = self.tokenize(row)+ ['\n']
+      line_tokens_cc = self.tokenize(row)
+      if True:
+        # TODO: do not add \n in the end!!
+        line_tokens_cc += ['\n']
+
       line_tokens = [s.lower() for s in line_tokens_cc]
       tokens_cc += line_tokens_cc
       tokens += line_tokens
