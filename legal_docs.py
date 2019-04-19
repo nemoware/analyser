c682ec7063b9fd774f7d254104a1009bc5b12057
Artem Zaborskiy <compartia@gmail.com>
2019-04-19 23:50:32 +0700

    Less verbose

--- legal_docs.py
+++ legal_docs.py
@@ -951,8 +951,8 @@ def calculate_distances_per_pattern(doc: LegalDocument, pattern_factory: Abstrac
       distances_per_pattern_dict[pat.name] = dists
       c += 1
 
-  if verbosity > 0:
-    print(distances_per_pattern_dict.keys())
+  # if verbosity > 0:
+  #   print(distances_per_pattern_dict.keys())
   if (c == 0):
     raise ValueError('no pattern with prefix: ' + pattern_prefix)
 
