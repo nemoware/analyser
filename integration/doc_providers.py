6caf5ffd7a056bd83e38a8d2472976eed8eb9837
Artem Zaborskiy <compartia@gmail.com>
2019-09-26 01:59:24 +0800

    softly Getting rid of docx2txt

--- integration/doc_providers.py
+++ integration/doc_providers.py
@@ -1,11 +1,5 @@
 import glob
-import os
-import sys
-import warnings
 from typing import List, Generator
-
-import docx2txt
-from overrides import overrides
 
 
 class AbstractDocProvider:
@@ -23,41 +17,3 @@ class DirDocProvider(AbstractDocProvider):
     filenames += [file for file in glob.glob(filename_prefix + "**/*.docx", recursive=True)]
     filenames += [file for file in glob.glob(filename_prefix + "**/*.doc", recursive=True)]
     return sorted(filenames)
-
-
-class GDriveTestDocProvider(DirDocProvider):
-
-  @overrides
-  def getdocuments(self, query):
-    return self.read_documents(query)
-
-  def read_doc(self, fn):
-
-    text = ''
-    try:
-      text = docx2txt.process(fn)
-    except:
-      info_ = f"Cannot read file {fn} with docx2txt, error: {sys.exc_info()}"
-      warnings.warn(info_, RuntimeWarning)
-
-      try:
-        os.system('antiword -w 0 "' + fn + '" > "' + fn + '.txt"')
-        with open(fn + '.txt') as f:
-          text = f.read()
-      except:
-        exc_info_ = f"Cannot read file {fn} with antiword, error: {sys.exc_info()}"
-        warnings.warn(exc_info_, RuntimeWarning)
-
-    return text
-
-  def read_documents(self, filename_prefix):
-    filenames = self.list_filenames(filename_prefix)
-
-    for file in filenames:
-      try:
-        text = self.read_doc(file)
-        yield file, text
-
-      except:
-        info_ = f"Cannot read file {file}, error: {sys.exc_info()}"
-        warnings.warn(info_, RuntimeWarning)
