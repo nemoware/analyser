bbcb756804e5442db191b9cd423fa99b1a92cb1f
Artem Zaborskiy <compartia@gmail.com>
2019-09-25 14:35:01 +0800

    - fixing dir list

--- integration/doc_providers.py
+++ integration/doc_providers.py
@@ -20,9 +20,9 @@ class AbstractDocProvider:
 class DirDocProvider(AbstractDocProvider):
   def list_filenames(self, filename_prefix) -> List[str]:
     filenames = []
-    filenames += [file for file in glob.glob(filename_prefix + "*.docx", recursive=True)]
-    filenames += [file for file in glob.glob(filename_prefix + "*.doc", recursive=True)]
-    return filenames
+    filenames += [file for file in glob.glob(filename_prefix + "**/*.docx", recursive=True)]
+    filenames += [file for file in glob.glob(filename_prefix + "**/*.doc", recursive=True)]
+    return sorted(filenames)
 
 
 class GDriveTestDocProvider(DirDocProvider):
