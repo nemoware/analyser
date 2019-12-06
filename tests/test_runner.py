32982f9cc8ab5df16e9ce3aeb4521ec9e0d7ed21
Artem Zaborskiy <compartia@gmail.com>
2019-12-06 16:20:43 +0800

    Misc

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -8,6 +8,7 @@ import warnings
 
 from analyser.runner import *
 
+SKIP_DB = get_mongodb_connection() is None
 SKIP_TF = True
 
 
@@ -17,94 +18,76 @@ def get_runner_instance_no_embedder() -> Runner:
   return TestRunner.default_no_tf_instance
 
 
-@unittest.skipIf(get_mongodb_connection() is None, "requires mongo")
+@unittest.skipIf(SKIP_DB, "requires mongo")
 class TestRunner(unittest.TestCase):
   default_no_tf_instance: Runner = None
 
-  @unittest.skipIf(get_mongodb_connection() is None, "requires mongo")
+  @unittest.skipIf(SKIP_DB, "requires mongo")
   def test_get_audits(self):
     aa = get_audits()
     for a in aa:
       print(a['_id'])
 
-  @unittest.skipIf(get_mongodb_connection() is None, "requires mongo")
+  @unittest.skipIf(SKIP_DB, "requires mongo")
   def test_get_docs_by_audit_id(self):
     audit_id = next(get_audits())['_id']
     docs = get_docs_by_audit_id(audit_id, kind='PROTOCOL')
     for a in docs:
       print(a['_id'], a['filename'])
 
-  @unittest.skipIf(SKIP_TF, "requires TF")
-  def test_process_single_protocol(self):
-    audit = next(get_audits())
-    audit_id = audit['_id']
-    docs = get_docs_by_audit_id(audit_id, kind='PROTOCOL')
-    doc = next(docs)
+  def _get_doc_from_db(self, kind):
+    audits = get_mongodb_connection()['audits'].find().sort([("createDate", pymongo.ASCENDING)]).limit(1)
+    for audit in audits:
+      for doc in get_docs_by_audit_id(audit['_id'], kind=kind, state=3).limit(1):
+        print(doc['_id'])
+        yield doc
 
-    ProtocolProcessor().process(doc, audit)
+  def _preprocess_single_doc(self, kind):
+    for doc in self._get_doc_from_db(kind):
+      processor = document_processors.get(kind, None)
+      processor.preprocess(doc)
 
-  @unittest.skipIf(SKIP_TF, "requires TF")
-  def test_process_single_contract(self):
-    audit = next(get_audits())
-    audit_id = audit['_id']
-    docs = get_docs_by_audit_id(audit_id, kind='CONTRACT')
-    doc = next(docs)
-    doc = next(docs)
-    doc = next(docs)
+  # @unittest.skipIf(SKIP_TF, "requires TF")
 
-    ContractProcessor().process(doc, audit)
+  @unittest.skipIf(SKIP_DB, "requires mongo")
+  def test_preprocess_single_protocol(self):
+    self._preprocess_single_doc('PROTOCOL')
 
-  @unittest.skipIf(SKIP_TF, "requires TF")
-  def test_process_single_charter(self):
-    audit = next(get_audits())
-    audit_id = audit['_id']
-    docs = get_docs_by_audit_id(audit_id, kind='CHARTER')
-    doc = next(docs)
+  @unittest.skipIf(SKIP_DB is None, "requires mongo")
+  def test_preprocess_single_contract(self):
+    self._preprocess_single_doc('CONTRACT')
 
-    CharterProcessor().process(doc, audit)
-
+  @unittest.skipIf(get_mongodb_connection() is None, "requires mongo")
   def test_process_contracts_phase_1(self):
-    runner = get_runner_instance_no_embedder()
+    runner = Runner.get_instance()
 
-    for audit in get_audits():
-      audit_id = audit['_id']
-      docs = get_docs_by_audit_id(audit_id, kind='CONTRACT')
+    audit_id = next(get_audits())['_id']
+    docs = get_docs_by_audit_id(audit_id, kind='CONTRACT')
+    for doc in docs:
+      processor = document_processors.get('CONTRACT', None)
+      processor.preprocess(doc)
 
-      for doc in docs:
-        charter = runner.make_legal_doc(doc)
-        runner.contract_parser.find_org_date_number(charter)
-        save_analysis(doc, charter)
-        print(charter._id)
-
+  @unittest.skipIf(get_mongodb_connection() is None, "requires mongo")
   def test_process_charters_phase_1(self):
-    runner = get_runner_instance_no_embedder()
+    runner = Runner.get_instance()
 
-    for audit in get_audits():
-      audit_id = audit['_id']
-      docs = get_docs_by_audit_id(audit_id, kind='CHARTER')
-
-      for doc in docs:
-        charter = runner.make_legal_doc(doc)
-        print(charter._id)
-        runner.charter_parser.find_org_date_number(charter)
-        save_analysis(doc, charter)
+    audit_id = next(get_audits())['_id']
+    docs = get_docs_by_audit_id(audit_id, kind='CHARTER')
+    for doc in docs:
+      processor = document_processors.get('CHARTER', None)
+      processor.preprocess(doc)
 
   def test_process_protocols_phase_1(self):
     runner = get_runner_instance_no_embedder()
 
-    audit_id = next(get_audits())['_id']
-    docs = get_docs_by_audit_id(audit_id, kind='PROTOCOL')
+    for audit in get_audits():
+      audit_id = audit['_id']
+      docs = get_docs_by_audit_id(audit_id, kind='PROTOCOL')
 
-    for doc in docs:
-      charter = runner.make_legal_doc(doc)
-      runner.protocol_parser.find_org_date_number(charter)
-      save_analysis(doc, charter)
-
-  # def test_process_single_contract(self):
-  #   audit_id = next(get_audits())['_id']
-  #   docs = get_docs_by_audit_id(audit_id, kind='CONTRACT')
-  #   doc = next(docs)
-  #   process_document(doc)
+      for doc in docs:
+        charter = runner.make_legal_doc(doc)
+        runner.protocol_parser.find_org_date_number(charter)
+        save_analysis(doc, charter, -1)
 
 
 if get_mongodb_connection() is not None:
