07cafe23a4c749703b9a06400bc5ea18d9a7338f
Artem Zaborskiy <compartia@gmail.com>
2019-12-08 14:01:22 +0800

    Fix get_mongodb_connection

--- integration/db.py
+++ integration/db.py
@@ -4,57 +4,46 @@ import warnings
 from pymongo import MongoClient
 
 _db_client = None
-_db_name = None
 
 
-def __check_var(vname):
+def _env_var(vname, default_val=None):
   if vname not in os.environ:
-    msg = f'MongoDB could not be connected, define {vname} environment variable'
+    msg = f'MongoDB : define {vname} environment variable! defaulting to {default_val}'
     warnings.warn(msg)
-    return False
+    return default_val
   else:
-    return True
+    return os.environ[vname]
 
 
 # mongod --config /usr/local/etc/mongod.conf
-
 def get_mongodb_connection():
   global _db_client
-  global _db_name
+  db_name = _env_var('GPN_DB_NAME', 'gpn')
   if _db_client is None:
     try:
-      if __check_var('GPN_DB_HOST') and __check_var('GPN_DB_PORT') and __check_var('GPN_DB_NAME'):
-        _db_name = os.environ["GPN_DB_NAME"]
-        _db_client = MongoClient(f'mongodb://{os.environ["GPN_DB_HOST"]}:{os.environ["GPN_DB_PORT"]}/')
-        _db_client.server_info()
-        return _db_client[_db_name]
+      host = _env_var('GPN_DB_HOST', 'localhost')
+      port = _env_var('GPN_DB_PORT', 27017)
+
+      _db_client = MongoClient(f'mongodb://{host}:{port}/')
+      _db_client.server_info()
+
     except Exception as err:
       _db_client = None
       warnings.warn(err)
-  else:
-    return _db_client[_db_name]
-
-  warnings.warn('defaulting MongoDB to mongodb://localhost:27017/')
-  return _get_local_mongodb_connection()
+  return _db_client[db_name]
 
 
 def _get_local_mongodb_connection():
-  global _db_client
-  global _db_name
-  _db_name = 'gpn'
-  if _db_client is None:
-    try:
-      _db_client = MongoClient(f'mongodb://localhost:27017/')
-      _db_client.server_info()
-      return _db_client[_db_name]
-    except Exception as err:
-      _db_client = None
-      msg=f'{err}'
-      warnings.warn(msg)
-  else:
-    return _db_client[_db_name]
+  try:
+    _db_client = MongoClient(f'mongodb://localhost:27017/')
+    _db_client.server_info()
+    return _db_client['gpn']
+  except Exception as err:
+    msg = f'{err}'
+    warnings.warn(msg)
   return None
 
 
 if __name__ == '__main__':
-  get_mongodb_connection()
+  x = get_mongodb_connection()
+  assert x is not None
