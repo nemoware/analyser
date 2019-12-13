93b8a191973008818528973ca2ff90b7c256be09
Artem Zaborskiy <compartia@gmail.com>
2019-12-13 23:47:36 +0800

    Removing unused code

--- analyser/structures.py
+++ analyser/structures.py
@@ -2,20 +2,6 @@
 # -*- coding: utf-8 -*-
 # coding=utf-8
 from enum import Enum, unique, EnumMeta
-from typing import List
-
-from analyser.ml_tools import TokensWithAttention
-
-ORG_TYPES = {
-  'Акционерное общество',
-  'Закрытое акционерное общество',
-  'Открытое акционерное общество',
-
-  'Государственное автономное учреждение',
-  'Муниципальное бюджетное учреждение',
-  'Некоммерческое партнерство',
-  'Федеральное государственное унитарное предприятие'
-}
 
 legal_entity_types = {
   'Акционерное общество': 'АО',
@@ -79,18 +65,6 @@ class OrgStructuralLevel(Enum, metaclass=DisplayStringEnumMeta):
 
 ORG_LEVELS_names = [x.display_string for x in OrgStructuralLevel]
 
-ORG_2_ORG = {
-  'all': OrgStructuralLevel.ShareholdersGeneralMeeting,
-  'gen': OrgStructuralLevel.CEO,
-  'directors': OrgStructuralLevel.BoardOfDirectors,
-  'pravlenie': OrgStructuralLevel.BoardOfCompany,
-  # TODO: what?
-  'head.all': OrgStructuralLevel.ShareholdersGeneralMeeting,
-  'head.gen': OrgStructuralLevel.CEO,
-  'head.directors': OrgStructuralLevel.BoardOfDirectors,
-  'head.pravlenie': OrgStructuralLevel.BoardOfCompany
-}
-
 
 @unique
 class ContractTags(Enum, metaclass=DisplayStringEnumMeta):
@@ -120,125 +94,4 @@ class CharterSubject(Enum, metaclass=DisplayStringEnumMeta):
   RealEstate = 4, 'Недвижимость'
   Insurance = 5, 'Страхование'
   Consulting = 6, 'Консультационные услуги'
-
-
-class Citation:
-  def __init__(self, cite: TokensWithAttention = None) -> None:
-    self.citation = cite
-
-
-class CharterConstraint(Citation):
-  def __init__(self, upper, lower, subject: ContractSubject, level: OrgStructuralLevel, *args, **kwargs) -> None:
-    super(CharterConstraint, self).__init__(*args, **kwargs)
-    self.upper, self.lower = upper, lower
-    self.subject = subject
-    self.level = level
-
-  def __repr__(self) -> str:
-    return f'Ограничение: >{self.lower} <{self.upper} :{self.level.display_string}'
-
-  def in_range(self, value):
-    return self.upper <= value <= self.lower
-
-
-class Charter(Citation):
-  def __init__(self, org_name, constraints: List[CharterConstraint], date=None, *args, **kwargs) -> None:
-    super(Charter, self).__init__(*args, **kwargs)
-    self.org_name = org_name
-    self.date = date
-    self.constraints = constraints
-
-  # sorted!
-  def find_constraints(self, subject: ContractSubject, value=None):
-    l = []
-    if value is None:
-      l = [c for c in self.constraints if c.subject == subject]
-    else:
-      l = [c for c in self.constraints if c.subject == subject and c.in_range(value)]
-
-    return sorted(l, key=lambda c: c.subject.value, reverse=True)
-
-
-class Contract(Citation):
-  def __init__(self, org_name: str, subject: ContractSubject, sum: float, contractor_name: str = None,
-               date=None, *args, **kwargs) -> None:
-    super(Contract, self).__init__(*args, **kwargs)
-    self.org_name = org_name
-    self.subject = subject
-    self.value = sum
-
-    self.contractor_name = contractor_name  # FFU
-    self.date = date  # FFU
-
-
-class Protocol(Contract):
-  def __init__(self, *args, **kwargs) -> None:
-    super(Protocol, self).__init__(*args, **kwargs)
-
-
-class FinalViolationLog:
-
-  def __init__(self, charter: Charter) -> None:
-    self.charter = charter
-
-    self.need_protocol = False
-    self.has_violations = False
-
-  def check_contract(self, contract: Contract):
-    self.contract = contract
-
-    contract_constraint: List[CharterConstraint] = self.charter.find_constraints(self.contract.subject,
-                                                                                 self.contract.value)
-
-    if contract_constraint:
-      if contract_constraint[0].level > OrgStructuralLevel.CEO:
-        self.need_protocol = True
-        print('Нужен протокол!')  # TODO debug print
-      else:
-        self.has_violations = False
-        print(f'Нарушений нет: '
-              f'Уровень:{contract_constraint[0].level.display_string} '
-              f'сумма: {contract_constraint[0].lower}<{self.contract.value}<{contract_constraint[0].upper}')  # TODO debug print
-    else:
-      self.has_violations = True
-      print(
-        f'Нарушение(?): Не найдено ни одного ограничения для суммы договора в {self.contract.value}')  # TODO debug print
-
-  def check_protocol(self, protocol: Protocol):
-    self.protocol = protocol
-
-    if self.contract.subject == self.protocol.subject:
-
-      if self.contract.value <= self.protocol.value:
-        print('Нарушений нет!')
-        return True
-      else:
-        print('Сумма протокола меньше суммы договора!')
-        return False
-
-    else:
-      print('Предмет договора и контракта не совпадают!')
-
-  def render(self):
-    pass
-
-
-if __name__ == '__main__':
-  l = [
-    CharterConstraint(1000, 100, ContractSubject.Charity, OrgStructuralLevel.BoardOfCompany, cite=None),
-    CharterConstraint(10, 1, ContractSubject.Other, OrgStructuralLevel.ShareholdersGeneralMeeting, cite=None),
-    CharterConstraint(100, 10, ContractSubject.Charity, OrgStructuralLevel.CEO, cite=None),
-    CharterConstraint(100, 10, ContractSubject.Charity, OrgStructuralLevel.ShareholdersGeneralMeeting, cite=None),
-  ]
-
-  c = Charter('dd', l, cite=None)
-  print(c.find_constraints(ContractSubject.Charity))
-
-  print(f'{ContractSubject.Charity.name}')
-  print(f'{ContractSubject.Charity.display_string}')
-  print('All:')
-  for subj in ContractSubject:
-    print(f'{subj.display_string}')
-
-  for level in OrgStructuralLevel:
-    print(f'{level.display_string}')
+  
