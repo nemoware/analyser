bfe60a43ca85812c341d00c6d6af642a553db7a8
Artem Zaborskiy <compartia@gmail.com>
2020-05-25 09:02:37 +0800

    + Все ДО option

--- analyser/runner.py
+++ analyser/runner.py
@@ -83,7 +83,7 @@ class BaseProcessor:
     else:
       date_is_ok = True
 
-    return ("Все ДО" == audit["subsidiary"]["name"] or legal_doc.is_same_org(
+    return ("* Все ДО" == audit["subsidiary"]["name"] or legal_doc.is_same_org(
       audit["subsidiary"]["name"])) and date_is_ok
 
 
