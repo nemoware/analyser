cad48832bf917cb4d25dae35b57c65426fc22591
Sergey Shalaevsky <shalaevskiy@nemosoft.ru>
2020-12-21 17:40:16 +0300

    added: big deal logic for all members case

--- analyser/finalizer.py
+++ analyser/finalizer.py
@@ -303,15 +303,25 @@ def check_contract(contract, charters, protocols, audit, supplementary_agreement
         contract_value = None
         if contract_attrs.get("sign_value_currency/value") is not None and contract_attrs.get("sign_value_currency/currency") is not None:
             contract_value = convert_to_rub({"value": contract_attrs["sign_value_currency/value"]["value"], "currency": contract_attrs["sign_value_currency/currency"]["value"]})
-        if contract_value is not None and audit.get('bookValues') is not None:
-            bookValue = get_book_value(audit, str(contract_attrs["date"]["value"].year - 1))
-            if bookValue is not None:
-                if bookValue * 0.25 < contract_value["value"] <= bookValue * 0.5:
-                    competences = {'BoardOfDirectors': {"min": bookValue * 0.25, "original_min": 25, "original_currency_min": "%", "max": bookValue * 0.5, "original_max": 50, "original_currency_max": "%", "competence_attr_name": 'BoardOfDirectors/BigDeal'}}
-                    change_contract_primary_subject(contract, 'BigDeal')
-                elif contract_value["value"] > bookValue * 0.5:
-                    competences = {'ShareholdersGeneralMeeting': {"min": bookValue * 0.5, "original_min": 50, "original_currency_min": "%", "max": np.inf, "competence_attr_name": 'ShareholdersGeneralMeeting/BigDeal'}}
-                    change_contract_primary_subject(contract, 'BigDeal')
+
+            if contract_value is not None and audit.get('bookValues') is not None:
+                bookValue = get_book_value(audit, str(contract_attrs["date"]["value"].year - 1))
+                if bookValue is not None:
+                    if eligible_charter_attrs.get('org-1-type') is not None and 'акционерное общество' in eligible_charter_attrs['org-1-type']['value'].lower():
+                        if bookValue * 0.25 < contract_value["value"] <= bookValue * 0.5:
+                            competences = {'BoardOfDirectors': {"min": bookValue * 0.25, "original_min": 25, "original_currency_min": "%", "max": bookValue * 0.5, "original_max": 50, "original_currency_max": "%", "competence_attr_name": 'BoardOfDirectors/BigDeal'}}
+                            change_contract_primary_subject(contract, 'BigDeal')
+                        elif contract_value["value"] > bookValue * 0.5:
+                            competences = {'ShareholdersGeneralMeeting': {"min": bookValue * 0.5, "original_min": 50, "original_currency_min": "%", "max": np.inf, "competence_attr_name": 'ShareholdersGeneralMeeting/BigDeal'}}
+                            change_contract_primary_subject(contract, 'BigDeal')
+                    else:
+                        if charter_subject_map.get('BigDeal') is not None and charter_subject_map['BigDeal'].get('AllMembers') is not None:
+                            limit = charter_subject_map['BigDeal']['AllMembers']['min']
+                            if charter_subject_map['BigDeal']['AllMembers']['original_currency_min'] == 'Percent':
+                                limit = charter_subject_map['BigDeal']['AllMembers']['min'] * bookValue / 100
+                            if contract_value['value'] > limit:
+                                competences = {'AllMembers': charter_subject_map['BigDeal']['AllMembers']}
+                                change_contract_primary_subject(contract, 'BigDeal')
 
         if competences is not None and contract_value is not None:
             eligible_protocol = None
